1e60772a80fe65220beac73c6059ff4350059194
Owen Ripley <ojripley@gmail.com>
2020-01-31 12:38:12 -0500

    add camera functionality

--- App.js
+++ App.js
@@ -1,15 +1,57 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { render } from 'react-dom';
+import { Camera } from 'expo-camera';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+export default function App() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    console.log('asking for permission');
+    (async () => {
+      console.log('butnothingishappening');
+      const { status } = await Camera.requestPermissionsAsync();
+      console.log(status);
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View></View>;
   }
+  if (hasPermission === false) {
+    return <Text>Cannot access camera.</Text>
+  }
+  return (
+    <View style={{ flex: 1}}>
+      <Camera style={{flex: 1,}} type={cameraType}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              setCameraType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
